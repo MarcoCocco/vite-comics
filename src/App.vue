749f11f80f2628358d82d1976fc90bbcccab5b1e
MarcoCocco <marcococco28@gmail.com>
2023-03-24 14:37:03 +0100

    Init exercise

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <script>
+import AppNavbar from "./components/AppNavbar.vue"
+import AppJumbotron from "./components/AppJumbotron.vue"
+import AppMain from "./components/AppMain.vue"
+import AppFooter from "./components/AppFooter.vue"
 
 export default {
   data() {
     return {
 
     }
+  },
+
+  components: {
+    AppNavbar,
+    AppJumbotron,
+    AppMain,
+    AppFooter
   }
+
 }
 </script>
 
 <template>
-<h1>ciao</h1>
+
+<AppNavbar></AppNavbar>
+
+<AppJumbotron></AppJumbotron>
+
+<AppMain></AppMain>
+
+<AppFooter></AppFooter>
+
 </template>
 
 <style lang='scss' scoped>
-
 
 </style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+<h1>tu?</h1>
+</template>
+
+<style lang='scss' scoped>
+
+</style>

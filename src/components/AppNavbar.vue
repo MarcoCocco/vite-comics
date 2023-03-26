5ac75cefb0b2b83bda8d492b5f5ae79af696a899
MarcoCocco <marcococco28@gmail.com>
2023-03-26 12:13:58 +0200

    feat: stilizzata navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -4,15 +4,71 @@ export default {
   data() {
     return {
 
+      links: [
+        'characters',
+        'comics',
+        'movies',
+        'tv',
+        'games',
+        'collectibles',
+        'videos',
+        'fans',
+        'news',
+        'shop',
+      ]
+
     }
   }
 }
 </script>
 
 <template>
-<h1>ciao</h1>
+  <nav>
+    <div class="navbar-container container-centered">
+
+      <div class="logo">
+        <img src="/images/dc-logo.png" alt="DC Logo">
+      </div>
+      <div class="links">
+        <ul>
+          <li v-for="link in links"><strong>{{ link }}</strong></li>
+        </ul>
+      </div>
+
+    </div>
+  </nav>
 </template>
 
 <style lang='scss' scoped>
+nav {
+  background-color: white;
+  color: #464646;
 
+  .navbar-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo {
+      padding: 20px;
+    }
+
+    .links ul {
+      display: flex;
+      flex-direction: row;
+      gap: 20px;
+
+      li {
+        text-transform: uppercase;
+        font-size: .8em;
+
+        &:hover {
+          cursor: pointer;
+          color: #0282f9;
+        }
+      }
+    }
+  }
+
+}
 </style>

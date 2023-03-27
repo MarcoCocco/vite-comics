9d0bb6a57dd6a0c261d05aea701ac8030202dfa3
MarcoCocco <marcococco28@gmail.com>
2023-03-27 16:48:30 +0200

    feat: aggiunte le card con relative info

---
+++ src/components/AppCard.vue
@@ -0,0 +1,55 @@
+<script>
+
+export default {
+    name: 'AppCard',
+
+    data() {
+        return {
+
+        }
+    },
+
+    props: {
+        thumb: String,
+        price: String,
+        series: String,
+        type: String,
+    },
+}
+</script>
+
+<template>
+    <div id="card">
+        <img :src="thumb" alt="">
+        <div id="series">
+            <p>{{ series }}</p>
+        </div>
+        <div id="type">
+            <p>{{ type }}</p>
+        </div>
+        <div id="price">
+            <p>{{ price }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+#card {
+    width: calc(100% / 6 - 20px);
+    display: flex;
+    flex-direction: column;
+    font-size: .8em;
+
+    #series {
+        font-weight: bold;
+        font-size: 1.2em;
+    }
+
+    img {
+        height: 200px;
+        object-fit: cover;
+        object-position: top;
+    }
+
+}
+</style>
